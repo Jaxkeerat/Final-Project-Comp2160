ef795c711e48b3e4f0a113e2ff92ca445dd8f758
Hollowbox <46541577+Hollowbox@users.noreply.github.com>
2021-03-15 00:37:19 -0700

    current prod java and xml creation

--- app/src/main/java/com/example/finalproject/CurrentProducts.java
+++ app/src/main/java/com/example/finalproject/CurrentProducts.java
@@ -1,4 +1,14 @@
 package com.example.finalproject;
 
-public class CurrentProducts {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CurrentProducts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.current_products);
+    }
 }
