b43fd3b7e85f0ac428058a12818e072ad7da8c3e
Prabikjot-Singh <68027007+Prabikjot-Singh@users.noreply.github.com>
2021-03-19 22:13:50 -0700

    some changes to xml file

---
+++ app/src/main/java/com/example/finalproject/add_new_item.java
@@ -0,0 +1,12 @@
+package com.example.finalproject;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class add_new_item extends AppCompatActivity {
+
+
+}
