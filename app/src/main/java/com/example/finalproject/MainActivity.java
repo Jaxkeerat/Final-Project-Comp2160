7c0951a999165ee872d96a7c122622ed02e568d2
Jas Kirat Singh <jaskiratsingh31@gmail.com>
2021-03-29 11:25:46 -0700

    Fixing some errors

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    protected Button addButton ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addButton = (Button )findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addButtonActivity();
+            }
+        });
+    }
+    public void addButtonActivity(){
+        Intent intent = new Intent(this, add_new_item.class);
+        startActivity(intent);
     }
 }
