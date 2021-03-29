8816a8db2c8e9d84f4f3a8fba0d3aedc5661acd0
Hollowbox <46541577+Hollowbox@users.noreply.github.com>
2021-03-29 16:25:15 -0700

    Firebase Auto Populate ListView in Current prod snapshot code

--- app/src/main/java/com/example/finalproject/CurrentProducts.java
+++ app/src/main/java/com/example/finalproject/CurrentProducts.java
@@ -1,5 +1,6 @@
 package com.example.finalproject;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -17,6 +18,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -108,5 +110,65 @@ public class CurrentProducts extends AppCompatActivity {
 
         return combinedStr;
     }
+
+    /*
+    private void initializeListView() {
+        // creating a new array adapter for our list view.
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, listItems);
+
+        // below line is used for getting reference
+        // of our Firebase Database.
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        // in below line we are calling method for add child event
+        // listener to get the child of our database.
+        reference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                // this method is called when new child is added to
+                // our data base and after adding new child
+                // we are adding that item inside our array list and
+                // notifying our adapter that the data in adapter is changed.
+                listItems.add(snapshot.getValue(String.class));
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                // this method is called when the new child is added.
+                // when the new child is added to our list we will be
+                // notifying our adapter that data has changed.
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                // below method is called when we remove a child from our database.
+                // inside this method we are removing the child from our array list
+                // by comparing with it's value.
+                // after removing the data we are notifying our adapter that the
+                // data has been changed.
+                listItems.remove(snapshot.getValue(String.class));
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                // this method is called when we move our
+                // child in our database.
+                // in our code we are note moving any child.
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // this method is called when we get any
+                // error from Firebase with error.
+            }
+        });
+        // below line is used for setting
+        // an adapter to our list view.
+        currentProd.setAdapter(adapter);
+    }
+     */
 
 }
